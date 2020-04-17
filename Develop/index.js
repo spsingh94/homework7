7d2d466160e610302f7f273363ecf1833fd94133
Shaminder Singh <spsingh94.ss@gmail.com>
2020-04-17 19:12:17 -0400

    npm installed packages and created prompts.

---
+++ Develop/index.js
@@ -0,0 +1,21 @@
+//require all of the modules
+const fs = require("fs");
+const inquirer = require("inquirer");
+const axious = require("axious");
+
+//prompt user to input username and questions about their project
+const questions = [
+    prompt("Please enter your username"),
+    prompt("Please enter your project title"),
+    prompt("Please enter a small description about your project"),
+    prompt("Please enter questions you may have about your project"),
+];
+
+function writeToFile(README.md, data) {
+}
+
+function init() {
+
+}
+
+init();
