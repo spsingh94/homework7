d3f0f4052f969c7e8adf3371a293c6c6ed8115aa
Shaminder Singh <spsingh94.ss@gmail.com>
2020-04-17 20:25:46 -0400

    test on axios successful

--- Develop/index.js
+++ Develop/index.js
@@ -1,21 +1,57 @@
 //require all of the modules
 const fs = require("fs");
 const inquirer = require("inquirer");
-const axious = require("axious");
+const axios = require("axios");
 
 //prompt user to input username and questions about their project
-const questions = [
-    prompt("Please enter your username"),
-    prompt("Please enter your project title"),
-    prompt("Please enter a small description about your project"),
-    prompt("Please enter questions you may have about your project"),
-];
+// inquirer
+//     .prompt([
+//         "Enter username",
+//         "Enter email address",
+//     ])
+//     .then((answers) => {
+//         console.log(answers);
+//     })
+//     .catch(error => {
+//         if (error) {
+//             console.log("error")
+//         } else {
+//             console.log("success")
+//         }
+//     });
 
-function writeToFile(README.md, data) {
-}
+// const username = 
 
-function init() {
+axios
+.get(`https://api.github.com/users/spsingh94/repos?per_page=100`)
+.then((res) => {
+    console.log(res);
+}) 
+.catch(error =>{
+    if(error){
+        console.log("error");
+    } else {
+        console.log("success");
+    }
+});
 
-}
+// fs.readFile(function(element){
+//     console.log(element.data)
+// });
 
-init();
+
+// const questions = [
+//     prompt("Please enter your username"),
+//     prompt("Please enter your project title"),
+//     prompt("Please enter a small description about your project"),
+//     prompt("Please enter questions you may have about your project"),
+// ];
+
+// function writeToFile(README.md, data) {
+// }
+
+// function init() {
+
+// }
+
+// init();
